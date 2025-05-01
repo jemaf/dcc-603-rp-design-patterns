e46b9ef8b2341873f3d5d6a90474a78ca3c86c44
davi <davi.araujo@onfly.com.br>
2025-05-01 16:23:20 -0300

    implement decorator

---
+++ src/main/java/decorator/Bacon.java
@@ -0,0 +1,32 @@
+package decorator;
+
+public class Bacon extends IngredienteDecorator {
+    public Bacon(Ingrediente ingrediente) {
+        super(ingrediente);
+    }
+
+
+    public Bacon() {
+        super(null);
+    }
+
+    @Override
+    public String imprimeIngrediente() {
+        String ingredientes = getClass().getSimpleName();
+
+        if (super.ingrediente != null)
+            ingredientes += ", " + super.ingrediente.imprimeIngrediente();
+
+        return ingredientes;
+    }
+
+    @Override
+    public double valorDoIngrediente() {
+        double valor = 1.50;
+
+        if (super.ingrediente != null)
+            valor += super.ingrediente.valorDoIngrediente();
+
+        return valor;
+    }
+}
