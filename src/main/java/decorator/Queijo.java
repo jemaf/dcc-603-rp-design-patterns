ee115520bb8044406a11826f39fa657d3c26bdf4
Letícia S. Lenzoni <125830543+leticiascofield@users.noreply.github.com>
2025-05-02 19:32:21 -0300

    Decorator done

---
+++ src/main/java/decorator/Queijo.java
@@ -0,0 +1,31 @@
+package decorator;
+
+public class Queijo extends IngredienteDecorator{
+    public Queijo(Ingrediente ingrediente) {
+        super(ingrediente);
+    }
+
+    public Queijo() {
+        super(null);
+    }
+
+    @Override
+    public String imprimeIngrediente() {
+        String ingredientes = "Queijo";
+
+        if (super.ingrediente != null)
+            ingredientes += ", " + super.ingrediente.imprimeIngrediente();
+
+        return ingredientes;
+    }
+
+    @Override
+    public double valorDoIngrediente() {
+        double valor = 0.75;
+
+        if (super.ingrediente != null)
+            valor += super.ingrediente.valorDoIngrediente();
+
+        return valor;
+    }
+}
