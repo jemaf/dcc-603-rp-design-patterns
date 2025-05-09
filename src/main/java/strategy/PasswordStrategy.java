98e263f41a8f531da4de326e65efb469009cfd1f
Lucas Werlang <lucaswewe2806@gmail.com>
2025-05-08 21:51:52 -0300

    Implementação de LengthStrategy

--- src/main/java/strategy/PasswordStrategy.java
+++ src/main/java/strategy/PasswordStrategy.java
@@ -2,5 +2,15 @@ package strategy;
 
 public abstract class PasswordStrategy {
 
+    // Estratégia para validar o tamanho mínimo da senha (8 caracteres)
+    public class LengthStrategy extends PasswordStrategy {
+
+        @Override
+        public boolean validate(String password) {
+            return password.length() >= 8;
+        }
+    }
+
+
     public abstract boolean validate(String password);
 }
