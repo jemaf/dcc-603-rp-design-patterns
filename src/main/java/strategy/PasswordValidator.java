84907fca72e5ea157be05a8eae1c52c73360a1fd
Allan Dayrell <allan.dayrell@gmail.com>
2025-05-11 17:38:11 -0300

    strategy

--- src/main/java/strategy/PasswordValidator.java
+++ src/main/java/strategy/PasswordValidator.java
@@ -2,9 +2,13 @@ package strategy;
 
 public class PasswordValidator {
 
-    // FIXME só aceita um tipo de validação (por tamanho)
-    public boolean validate(String password) {
-        return password.length() >= 8;
+    private PasswordStrategy strategy;
+
+    public void setStrategy(PasswordStrategy strategy) {
+        this.strategy = strategy;
     }
 
+    public boolean validate(String password) {
+        return this.strategy.validate(password);
+    }
 }
