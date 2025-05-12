8a6af029f5e496d66904d4e8eb12ac1b99d55ba6
Carolina Pilla <carol.moyapilla@gmail.com>
2025-05-12 09:53:12 -0300

    Decorator implementado e passando nos testes

--- src/main/java/decorator/Main.java
+++ src/main/java/decorator/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Ingrediente produto = new Pao(new Pao());
+        Ingrediente produto1 = new Pao(new Pao());
+        Ingrediente produto2 = new Ketchup(new Ketchup());
+        Ingrediente produto3 = new Queijo(new Queijo());
+        Ingrediente produto4 = new Pao(new Pao());
 
-        System.out.println(produto.imprimeIngrediente());
-        System.out.println(produto.valorDoIngrediente());
-
+        // pao
+        System.out.println(produto1.imprimeIngrediente());
+        System.out.println(produto1.valorDoIngrediente());
     }
 }
