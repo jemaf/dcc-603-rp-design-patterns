a829529ec486857dbbcd241f80e310ada2c10907
pollybrandao <pollyannybrandao@gmail.com>
2025-05-11 23:20:54 -0300

    Padrão Observer: Implementação dos métodos da classe Subject. Testado

--- src/main/java/decorator/Main.java
+++ src/main/java/decorator/Main.java
@@ -6,7 +6,12 @@ public class Main {
 
         Ingrediente produto = new Pao(new Pao());
 
+        Ingrediente sanduiche1 = new Salada(new Queijo(new Pao()));
+
         System.out.println(produto.imprimeIngrediente());
         System.out.println(produto.valorDoIngrediente());
+
+        System.out.println(sanduiche1.imprimeIngrediente());
+        System.out.println(sanduiche1.valorDoIngrediente());
     }
 }
