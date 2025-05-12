b6f47934328342697df5f50d03db33e3961427f7
BrunoSilva14 <brunosoaresbd@hotmail.com>
2025-05-12 18:12:11 -0300

    Resolução de roteiro prático 1

---
+++ src/main/java/decorator/Ketchup.java
@@ -0,0 +1,28 @@
+package decorator;
+
+public class Ketchup extends IngredienteDecorator {
+
+    public Ketchup(Ingrediente ingrediente) {
+        super(ingrediente);
+    }
+
+    public Ketchup() {
+        super(null);
+    }
+
+    @Override
+    public String imprimeIngrediente() {
+        return "Ketchup" + ", " + ingrediente.imprimeIngrediente();
+    }
+
+    @Override
+    public double valorDoIngrediente() {
+
+        double valor = 1.00;
+
+        if (super.ingrediente != null)
+            valor += ingrediente.valorDoIngrediente();
+
+        return valor;
+    }
+}
