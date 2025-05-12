a0b09d1a8871a65e2de53b1f90b3ac81dc15fe20
Pedro Henrique de Mattos Gomes <pedromattosg@hotmail.com>
2025-05-11 23:36:43 -0300

    Implementando a classe Queijo para o Decorator

---
+++ src/main/java/decorator/Queijo.java
@@ -0,0 +1,19 @@
+package decorator;
+
+public class Queijo extends IngredienteDecorator {
+
+    public Queijo(Ingrediente ingrediente) {
+        super(ingrediente);
+    }
+
+    @Override
+    public String imprimeIngrediente() {
+        return super.imprimeIngrediente() + ", Queijo";
+    }
+
+    @Override
+    public double valorDoIngrediente() {
+        return super.valorDoIngrediente() + 0.75;
+    }
+
+}
