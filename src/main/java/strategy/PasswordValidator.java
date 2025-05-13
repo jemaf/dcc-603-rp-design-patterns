8195a7d3dbd75997806b20c11304e5be550b2041
Bedro Boseja <104467351+Pedro-Boseja@users.noreply.github.com>
2025-05-12 22:50:18 -0300

    Implementação dos padrões Singleton, Observer,Strategy e Decorator

--- src/main/java/strategy/PasswordValidator.java
+++ src/main/java/strategy/PasswordValidator.java
@@ -2,9 +2,17 @@ package strategy;
 
 public class PasswordValidator {
 
+    private PasswordStrategy strategy;
+
+    public void setStrategy(PasswordStrategy strategy) {
+        this.strategy = strategy;
+    }
     // FIXME só aceita um tipo de validação (por tamanho)
     public boolean validate(String password) {
-        return password.length() >= 8;
+        if (strategy == null) {
+            throw new IllegalStateException("Nenhuma estratégia definida!");
+        }
+        return strategy.validate(password);
     }
 
 }
