88d9df6058648d11c7d5cc4e69e561bcb5083548
Jonas F. Lopes <jonas.lopes@uemg.br>
2025-05-12 22:54:28 -0300

    feat: strategy pattern

---
+++ src/main/java/strategy/SpecialCharStrategy.java
@@ -0,0 +1,8 @@
+package strategy;
+
+public class SpecialCharStrategy extends PasswordStrategy {
+    @Override
+    public boolean validate(String password) {
+        return password.chars().anyMatch(ch -> "!@#$%^&*()_+".indexOf(ch) >= 0);
+    }
+}
