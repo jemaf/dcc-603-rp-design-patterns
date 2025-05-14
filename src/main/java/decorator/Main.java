aa8e1bb3ad102d61b5e28e8847ddd5c566e498cd
eg1to <pedro.h.egito@gmail.com>
2025-05-13 22:13:25 -0300

    updating decorator pack

--- src/main/java/decorator/Main.java
+++ src/main/java/decorator/Main.java
@@ -4,10 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Ingrediente produto = new Pao(new Pao());
+        Ingrediente pedido = new Salada(
+                new Queijo(
+                        new Bacon(
+                                new Ketchup(
+                                        new Pao()
+                                )
+                        )
+                )
+        );
 
-        System.out.println(produto.imprimeIngrediente());
-        System.out.println(produto.valorDoIngrediente());
-
+        System.out.println(pedido.imprimeIngrediente());
+        System.out.println(pedido.valorDoIngrediente());
     }
 }
+
